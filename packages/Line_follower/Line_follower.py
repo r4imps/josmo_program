422d2b79ece98319eecdb4f518f8505afe312ddf
raimo.holm <raimo.holm@eek.ee>
2022-12-07 16:35:52 +0200

    n'idis package, t;;tab, lisatud line follower

---
+++ packages/Line_follower/Line_follower.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import os
+import rospy
+from duckietown.dtros import DTROS, NodeType
+from std_msgs.msg import String
+from smbus2 import SMBus
+
+class MyPublisherNode(DTROS):
+
+    def __init__(self, node_name):
+        # initialize the DTROS parent class
+        super(MyPublisherNode, self).__init__(node_name=node_name, node_type=NodeType.GENERIC)
+        # construct publisher
+        self.pub = rospy.Publisher('sparkfun_line_array', String, queue_size=10)
+
+    def run(self):
+        # publish message every 1 second
+        rate = rospy.Rate(20) # 1Hz
+        while not rospy.is_shutdown():
+
+            SMBus(12).pec = 1  # Enable PEC
+            lanereader_value = SMBus(12).read_byte_data(0x3e, 0x11)
+
+            rospy.loginfo(f"Lanereader: '{lanereader_value}'")
+            self.pub.publish(str(lanereader_value))
+            rate.sleep()
+
+if __name__ == '__main__':
+    # create the node
+    node = MyPublisherNode(node_name='script')
+    # run node
+    node.run()
+    # keep spinning
+    rospy.spin()
