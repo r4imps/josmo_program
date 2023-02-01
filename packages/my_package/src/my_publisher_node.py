a910a354010a7ee6e0e6a2b77deee70a9e27c413
JostenLaanemets <josten11@hotmail.com>
2023-02-01 12:19:02 +0200

    Odomeetria arvutused

--- packages/my_package/src/my_publisher_node.py
+++ packages/my_package/src/my_publisher_node.py
@@ -48,6 +48,8 @@ class ROSPROG(DTROS):
     def on_shutdown(self):
         speed.vel_left = 0
         speed.vel_right = 0
+        Save_L_en=0
+        Save_R_en=0
         self.pub.publish(speed)
         rospy.on_shutdown()
     
@@ -56,44 +58,67 @@ class ROSPROG(DTROS):
 
     def run(self):
         rate = rospy.Rate(10)
-        so=0
-        Save_R_en=0
-        Display_R_en=0
-        Last_R_encoder=0
-        Save_L_en=0
-        Display_L_en=0
-        Last_L_encoder=0
-
+        flag=1
         R_Distance=0
         L_Distance=0
-        
+        Save_L_en=0
+        Save_R_en=0
+        Display_L_en=0
+        Display_R_en=0
         N_tot= 135
         R=0.03424
         baseline_wheel2wheel= 0.1
-
+        
+        Delta_null=0
+        Delta_A=0
+        Back2L= baseline_wheel2wheel
+        Wheel2R=R*R
+        Delta_x=0
+        Delta_y=0
 
         while not rospy.is_shutdown():
             read = SMBus(1).read_byte_data(0x3e, 0x11)
+            #Encoder Zeroing
+            if (self.L_encoder>0 or self.R_encoder>0) and flag == 0:
+                Display_L_en=self.L_encoder-Save_L_en
+                Display_R_en=self.R_encoder-Save_R_en
+
+
             
-            L_Rotation= self.L_encoder * ((2*np.pi)/N_tot)
+            elif flag == 1 and (self.L_encoder != 0 or self.R_encoder!=0):
+                Save_L_en = self.L_encoder
+                Save_R_en = self.R_encoder
+
+                flag = 0
+                                                            #rataste encoderite nullimine
+
+           
+            L_Rotation= Display_L_en * ((2*np.pi)/N_tot)
             print(f"The left wheel rotated: {np.rad2deg(L_Rotation)} degrees")
-            R_Rotation= self.R_encoder * ((2*np.pi)/N_tot)
+            R_Rotation= Display_R_en * ((2*np.pi)/N_tot)
             print(f"The right wheel rotated: {np.rad2deg(R_Rotation)} degrees")
             L_Distance= R*L_Rotation
             print(f"The left wheel travel: {round(L_Distance,3)} meters")
             R_Distance= R*R_Rotation
             print(f"The righ wheel travel: {round(R_Distance,3)} meters")
-            Distance= (L_Distance+R_Distance)/2
-            # encoder value
-            print(f"Distance traveled: {round(Distance ,3)} meters")
-            """if self.L_encoder>Save_L_en:
-                Display_L_en=self.L_encoder-Save_L_en
-                
 
-            if self.R_encoder>Save_R_en:
-                Display_R_en=self.R_encoder-Save_R_en
-            Save_L_en=self.L_encoder
-            Save_R_en=self.R_encoder"""
+            Delta_A=(R_Distance+L_Distance)/2
+            print(f"Delta distance: {round(Delta_A,3)}")
+            Delta_null= (R_Distance-L_Distance)/Back2L
+            print(f"Delta rotation: {round(Delta_null,3)}")
+
+            Delta_x=Delta_A*np.cos(Delta_null)
+            print(f"Delta X: {round(Delta_x,3)}")
+            Delta_y=Delta_A*np.sin(Delta_null)
+            print(f"Delta Y: {round(Delta_y,3)}")
+
+            #Distance= (L_Distance+R_Distance)/2
+            #print(f"Distance average travel: {round(Distance ,3)} meters")
+
+
+
+
+
                 
         
             #print("SO---"+(str(so)))
@@ -101,8 +126,8 @@ class ROSPROG(DTROS):
             #print("LAST L ENCODER: "+(str(N_tot)))
             #print("SAVE R: "+(str(Save_R_en)))
             #print("SAVE L: "+(str(Save_L_en)))
-            print("DISPLAY L ENCODER: "+(str(Display_L_en)))
             print("DISPLAY R ENCODER: "+(str(Display_R_en)))
+            print("DISPLAY L ENCODER: "+(str(Display_L_en)))
             #print(self.R_encoder)
             #print(self.L_encoder)
             #print("ToF Distance: "+(str(self.distance)))
