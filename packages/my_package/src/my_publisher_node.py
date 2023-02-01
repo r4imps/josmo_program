82fee56e701b3afbde028bd314b99f2752a5c488
r4imps <raimo2008@hotmail.com>
2023-02-01 10:50:25 +0200

    mahis 1

--- packages/my_package/src/my_publisher_node.py
+++ packages/my_package/src/my_publisher_node.py
@@ -1,49 +1,129 @@
 #!/usr/bin/env python3
-
 import os
 import rospy
-import time 
-from duckietown.dtros import DTROS, NodeType, TopicType
+import numpy as np
+from duckietown.dtros import DTROS, NodeType
 from std_msgs.msg import String
+from duckietown_msgs.msg import WheelsCmdStamped, WheelEncoderStamped
+from sensor_msgs.msg import Range
+import time
+from datetime import datetime
 from smbus2 import SMBus
-from duckietown_msgs.msg import WheelsCmdStamped, Twist2DStamped
+speed = WheelsCmdStamped()
+flag = 0
 
-class MyLine_followerNode(DTROS):
 
+
+
+
+
+
+class ROSPROG(DTROS):
     def __init__(self, node_name):
+
         # initialize the DTROS parent class
-        super(MyLine_followerNode, self).__init__(node_name=MyLine_followerNode, node_type=NodeType.GENERIC)
-        # construct publisher
-        self.pub = rospy.Publisher('sparkfun_line_follower', queue_size=10)
-        # Setup publishers
+        super(ROSPROG, self).__init__(node_name=node_name, node_type=NodeType.GENERIC)
+        # SUBSCRIBERID JA PUBLISHERID
+        self.pub = rospy.Publisher('/josmo/wheels_driver_node/wheels_cmd', WheelsCmdStamped, queue_size=10)
+        rospy.Subscriber('/josmo/front_center_tof_driver_node/range', Range, self.callback)
+
+        rospy.Subscriber('/josmo/right_wheel_encoder_node/tick', WheelEncoderStamped, self.Callback_R_Encoder)
+        rospy.Subscriber('/josmo/left_wheel_encoder_node/tick', WheelEncoderStamped, self.Callback_L_Encoder)
         
+        
+        self.distance=1.0
+        self.R_encoder=0
+        self.L_encoder=0
+        
+
+    def callback(self, data):
+
+        self.distance = data.range
+
+    def Callback_R_Encoder(self,data):
+        self.R_encoder = data.data
+
+    def Callback_L_Encoder(self,data):
+        self.L_encoder = data.data
+
+    def on_shutdown(self):
+        speed.vel_left = 0
+        speed.vel_right = 0
+        self.pub.publish(speed)
+        rospy.on_shutdown()
     
+      
+
 
     def run(self):
-        # publish message every 1 second
-        rate = rospy.Rate(20)
-        msg_wheels_cmd = WheelsCmdStamped()
-        while not rospy.is_shutdown():
+        rate = rospy.Rate(10)
+       
+        R_Distance=0
+        L_Distance=0
+        
+        N_tot= 135
+        R=0.03424
+        baseline_wheel2wheel= 0.1
 
-            self.lanereader_value = SMBus(1).read_byte_data(0x3e, 0x11)
-            rospy.loginfo(f"Lanereader: '{self.lanereader_value}'")
-            self.pub.publish(str(self.lanereader_value))
-             
+
+        while not rospy.is_shutdown():
+            read = SMBus(1).read_byte_data(0x3e, 0x11)
+
+            if self.L_encoder>0 and flag == 1:
+                Display_L_en=self.L_encoder-Save_L_en
+
+            
+            elif flag == 0 and self.L_encoder > 0:
+                Save_L_en = self.L_encoder
+                flag = 1
+                                                            #rataste encoderite nullimine
+            else:
+                continue
+
+            if self.R_encoder>0 and flag == 1:
+                Display_R_en=self.R_encoder-Save_R_en
+
+            
+            elif flag == 0 and self.R_encoder > 0:
+                Save_R_en = self.R_encoder
+                flag = 1
+
+            else:
+                continue
+            
+            L_Rotation= self.L_encoder * ((2*np.pi)/N_tot)
+            print(f"The left wheel rotated: {np.rad2deg(L_Rotation)} degrees")
+            R_Rotation= self.R_encoder * ((2*np.pi)/N_tot)
+            print(f"The right wheel rotated: {np.rad2deg(R_Rotation)} degrees")
+            L_Distance= R*L_Rotation
+            print(f"The left wheel travel: {round(L_Distance,3)} meters")
+            R_Distance= R*R_Rotation
+            print(f"The righ wheel travel: {round(R_Distance,3)} meters")
+            Distance= (L_Distance+R_Distance)/2
+            # encoder value
+            print(f"Distance traveled: {round(Distance ,3)} meters")
+
+            
+
+
+        
+            #print("SO---"+(str(so)))
+            #print("LAST R ENCODER: "+(str(Last_R_encoder)))
+            #print("LAST L ENCODER: "+(str(N_tot)))
+            #print("SAVE R: "+(str(Save_R_en)))
+            #print("SAVE L: "+(str(Save_L_en)))
+            print("DISPLAY L ENCODER: "+(str(Display_L_en)))
+            print("DISPLAY R ENCODER: "+(str(Display_R_en)))
+            #print(self.R_encoder)
+            #print(self.L_encoder)
+            #print("ToF Distance: "+(str(self.distance)))
+            print("Joon"+(str(read)))
+            self.pub.publish(speed)
 
             rate.sleep()
 
-class MyWheelsNode(DTROS):
-
-    def __init__(self) -> None:
-        super(MyWheelsNode,self).__init__(node, NodeType = NodeType.CONTROL)
-
-        self.pub = rospy.Publisher()
-
-
 if __name__ == '__main__':
-    # create the node
-    node = MyLine_followerNode(node_name='MyLine_followerNode')
-    # run node
+    node = ROSPROG(node_name='my_publisher_node')
     node.run()
-    # keep spinning
+    rospy.on_shutdown(ROSPROG.on_shutdown)
     rospy.spin()
