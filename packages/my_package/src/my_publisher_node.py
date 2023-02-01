f1a55a67d67515e195de9de82a58d467b2ddcabe
JostenLaanemets <josten11@hotmail.com>
2023-02-01 10:20:26 +0200

    01.02.2023 odo

--- packages/my_package/src/my_publisher_node.py
+++ packages/my_package/src/my_publisher_node.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import os
 import rospy
+import numpy as np
 from duckietown.dtros import DTROS, NodeType
 from std_msgs.msg import String
 from duckietown_msgs.msg import WheelsCmdStamped, WheelEncoderStamped
@@ -59,149 +60,53 @@ class ROSPROG(DTROS):
         Save_R_en=0
         Display_R_en=0
         Last_R_encoder=0
-
         Save_L_en=0
         Display_L_en=0
         Last_L_encoder=0
-        T_Start=0
-        T_Done=0    
-        Saved_Time=0.0
-        x=0
-   
+
+        R_Distance=0
+        L_Distance=0
+        
+        N_tot= 135
+        R=0.03424
+        baseline_wheel2wheel= 0.1
+
 
         while not rospy.is_shutdown():
             read = SMBus(1).read_byte_data(0x3e, 0x11)
-            Timer= time.time()
             
-
+            L_Rotation= self.L_encoder * ((2*np.pi)/N_tot)
+            print(f"The left wheel rotated: {np.rad2deg(L_Rotation)} degrees")
+            R_Rotation= self.R_encoder * ((2*np.pi)/N_tot)
+            print(f"The right wheel rotated: {np.rad2deg(R_Rotation)} degrees")
+            L_Distance= R*L_Rotation
+            print(f"The left wheel travel: {round(L_Distance,3)} meters")
+            R_Distance= R*R_Rotation
+            print(f"The righ wheel travel: {round(R_Distance,3)} meters")
+            Distance= (L_Distance+R_Distance)/2
             # encoder value
-            
-            if self.L_encoder!=Save_L_en:
-                Display_L_en=Display_L_en+1
-            Save_L_en=self.L_encoder    
-
-            if self.R_encoder!=Save_R_en:
-                Display_R_en=Display_R_en+1
-            Save_R_en=self.R_encoder
+            print(f"Distance traveled: {round(Distance ,3)} meters")
+            """if self.L_encoder>Save_L_en:
+                Display_L_en=self.L_encoder-Save_L_en
                 
-            if T_Start==1:
-                Time_set=3.0
-                x=Saved_Time+Time_set
-                if Timer == x:
-                    T_Done=1
 
-
-
-
-            if self.distance<0.3 and so==0:
-                speed.vel_right=0
-                speed.vel_left=0
-
-                so=10
-
-            #Keerab alguses
-            if so == 10 :
-                Last_R_encoder= Display_R_en
-                Last_L_encoder= Display_L_en
+            if self.R_encoder>Save_R_en:
+                Display_R_en=self.R_encoder-Save_R_en
+            Save_L_en=self.L_encoder
+            Save_R_en=self.R_encoder"""
                 
-                so=20
-
-            if so==20:
-                speed.vel_right=0.14
-                speed.vel_left=-0.14
-                if (Display_R_en>(Last_R_encoder+3)) and (Display_L_en>(Last_L_encoder+3)) :
-                    speed.vel_right=0
-                    speed.vel_left=0
-
-                    so=30                
-           #Otse
-            if so==30:
-                Last_R_encoder= Display_R_en
-                Last_L_encoder= Display_L_en
-                Saved_Time= Timer
-                T_Start=1
-                so=40
-                    
-            if so==40 and T_Done==1:
-                speed.vel_right=0.3
-                speed.vel_left=0.3
-                T_Start=0
-
-                if  (Display_R_en>(Last_R_encoder+10)) and (Display_L_en>(Last_L_encoder +10)):
-                    speed.vel_right=0
-                    speed.vel_left=0
-                    
-                    so=50
-            #Keerab paremale
-            if so == 50:
-                Last_R_encoder= Display_R_en
-                Last_L_encoder= Display_L_en
-                
-                so=60
-
-            if so==60:
-                speed.vel_right=-0.2
-                speed.vel_left=0.2
-                if (Display_R_en>(Last_R_encoder+2)) and (Display_L_en>(Last_L_encoder+2)) :
-                    speed.vel_right=0
-                    speed.vel_left=0
-                    so=70
-            #Otse          
-            if so==70:
-                Last_R_encoder= Display_R_en
-                Last_L_encoder= Display_L_en
-                
-                so=80
-                    
-            if so==80:
-                speed.vel_right=0.3
-                speed.vel_left=0.3
-
-                if  (Display_R_en>(Last_R_encoder+10)) and (Display_L_en>(Last_L_encoder+10)):
-                    speed.vel_right=0
-                    speed.vel_left=0
-                    
-                    so=90 
-            #Keerab paremale                                              
-            if so == 90:
-                Last_R_encoder= Display_R_en
-                Last_L_encoder= Display_L_en
-                
-                so=100
-
-            if so==100:
-                speed.vel_right=-0.2
-                speed.vel_left=0.2
-                if (Display_R_en>(Last_R_encoder+2)) and (Display_L_en>(Last_L_encoder+2)) :
-                    speed.vel_right=0
-                    speed.vel_left=0
-                    so=0                            
-                    
-
-
-      
-                            
-                
-            
-
-            print("SO---"+(str(so)))
-      
-            print("now SECS: "+ (str(Timer)))
-            print("done SECS: "+ (str(T_Done)))
-            print("done SECS: "+ (str(T_Done)))
-            print("saved SECS: "+ (str(x)))
-            print("Start SECS: "+ (str(T_Start)))
-
+        
+            #print("SO---"+(str(so)))
             #print("LAST R ENCODER: "+(str(Last_R_encoder)))
-            #print("LAST L ENCODER: "+(str(Last_L_encoder)))
+            #print("LAST L ENCODER: "+(str(N_tot)))
             #print("SAVE R: "+(str(Save_R_en)))
             #print("SAVE L: "+(str(Save_L_en)))
             print("DISPLAY L ENCODER: "+(str(Display_L_en)))
             print("DISPLAY R ENCODER: "+(str(Display_R_en)))
             #print(self.R_encoder)
             #print(self.L_encoder)
-            print("ToF Distance: "+(str(self.distance)))
-            #print("Joon"+(str(read)))
+            #print("ToF Distance: "+(str(self.distance)))
+            print("Joon"+(str(read)))
             self.pub.publish(speed)
 
             rate.sleep()
